60f2b7de29d6517da812f52b32966e04acdfc7d8
kmurphs <kibongesp@gmail.com>
2021-01-14 00:38:46 +0200

    Released current automated build to Github page

--- scripts/build.bat
+++ scripts/build.bat
@@ -7,29 +7,29 @@ CD ..\snake-game
 
 
 ECHO Delete current release folder content
-DEL /F/Q/S ..\tmp\docs\*.* > NUL
-RMDIR /Q/S ..\tmp\docs
 DEL /F/Q/S ..\tmp\*.* > NUL
-
+RMDIR /Q/S ..\tmp
 
 @REM Connect tmp to gh-pages
+MKDIR ..\tmp
 CD ..\tmp
 git init
 git remote add origin https://github.com/KMurphs/snake-game
-
-
+git fetch origin gh-pages
+git checkout -t origin/gh-pages
+git checkout -b mybranch
 
 ECHO Copy Github page files
 @REM Github Pages expect the entry point to be index.md or readme.md 
 ROBOCOPY ..\github-pages ..\tmp\docs\ /MIR /NFL /NDL 
 COPY ..\readme.md ..\tmp\docs
-COPY build\favicon.ico ..\tmp
+@REM COPY ..\snake-game\build\favicon.ico ..\tmp
 
 
 ECHO Copy release files
 @REM https://stackoverflow.com/a/7487697/9034699
 @REM MKDIR ..\docs\
-ROBOCOPY build ..\tmp\ /MIR /NFL /NDL 
+ROBOCOPY ..\snake-game\build ..\tmp\ /MIR /NFL /NDL 
 
 
 
